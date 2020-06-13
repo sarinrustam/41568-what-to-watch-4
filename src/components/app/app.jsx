1c1fc72788c5b37bf91e913e773669584a65154d
Rustam Sarin <lorienas@gmail.com>
2020-06-13 14:35:16 +0300

    added PropTypes package and setup it also refactoring filmsList section

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,25 @@
 import React from "react";
+
 import Main from "@components/main/main.jsx";
+import PropTypes from "prop-types";
 
 const App = (props) => {
-  // eslint-disable-next-line react/prop-types
-  const {title, genre, year} = props;
+  const {title, genre, year, films} = props;
   return (
-    <Main headerFilmTitle={title} headerFilmGenre={genre} headerFilmYear={year}/>
+    <Main
+      headerFilmTitle={title}
+      headerFilmGenre={genre}
+      headerFilmYear={year}
+      films={films}
+    />
   );
+};
+
+App.propTypes = {
+  title: PropTypes.string.isRequired,
+  genre: PropTypes.string.isRequired,
+  year: PropTypes.number.isRequired,
+  films: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
 import App from "@components/app/app.jsx";
 
 const FilmData = {
   TITLE: `The Grand Budapest Hotel`,
   GENRE: `Drama`,
-  YEAR: `2014`,
+  YEAR: 2014,
 };
+
+const FILMS = [`Fantastic Beasts`, `Bohemian Rhapsody`, `Macbeth`, `Avatar`, `Aviator`, `Pulp fiction`];
 
 ReactDOM.render(
     <App
       title={FilmData.TITLE}
       genre={FilmData.GENRE}
       year={FilmData.YEAR}
+      films={FILMS}
     />,
     document.querySelector(`#root`)
 );
