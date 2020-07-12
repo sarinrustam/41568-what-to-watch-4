90afacccee5d62241ee52d5c375a5dc888b3b185
Rustam Sarin <lorienas@gmail.com>
2020-07-12 21:06:28 +0300

    fixed

--- src/components/small-movie-card/small-movie-card.test.js
+++ src/components/small-movie-card/small-movie-card.test.js
@@ -24,9 +24,6 @@ const movie = {
   preview: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`
 };
 
-// const onMouseOverHandler = () => {};
-// const onMovieTitleClickHandler = () => {};
-
 describe(`Render SMC`, () => {
   it(`SMC should render movie`, () => {
     const tree = rerender
